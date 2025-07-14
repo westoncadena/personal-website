000ee8fb82a1ee77a56b5424088256b494418eea
westoncadena <westoncadena@gmail.com>
2025-07-14 11:31:43 -0500

    cleaned up .env variables and deprecated next.config.ts images.domains to images.remotePatterns

--- src/app/api/cloudinary-images/route.ts
+++ src/app/api/cloudinary-images/route.ts
@@ -3,7 +3,7 @@ import { v2 as cloudinary } from 'cloudinary';
 import { CloudinaryResource, PortfolioImage } from '@/lib/types';
 
 cloudinary.config({
-  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
   secure: true,
